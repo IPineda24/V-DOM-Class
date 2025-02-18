894e7552ebd178a3cbd560e4464143d2c9952543
Ipineda <irvin.pineda024@gmail.com>
2025-02-18 14:18:56 -0600

    Add conditionals example :memo:

---
+++ conditions.js
@@ -0,0 +1,10 @@
+let age = prompt("Ingresa tu edad");
+age = Number(age);
+
+if (age >= 18) {
+    alert("Es mayor de edad");
+} else if (age == 15) {
+    alert("Fiesta rosa!!!");
+} else {
+    alert("Ta chiquito");
+}
