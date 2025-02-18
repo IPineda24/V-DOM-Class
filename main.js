612fa83d3371d262b28b673d93c23bf9cb4839a0
Ipineda <irvin.pineda024@gmail.com>
2025-02-18 14:02:26 -0600

    add variables example :sparkles:

---
+++ main.js
@@ -0,0 +1,9 @@
+let name = "Mateo";  // string :)
+let age = 16;  // number :)
+let isStudent = true;  // boolean :)
+let direction = null; // object
+let phone; // undefined :(
+let hobbies = ["read", "play", "eat"]; // object
+let person = { name: "Aura", age: 17 } // object
+
+console.log(typeof person)
